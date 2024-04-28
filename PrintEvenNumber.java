0da1b1e30c6ba0502c29950661148de910bc1b39
Vishnoopriya <167874533+Vishnoopriya@users.noreply.github.com>
2024-04-28 18:05:39 +0530

    Add files via upload

---
+++ PrintEvenNumber.java
@@ -0,0 +1,25 @@
+package basicweb;
+
+
+public class PrintEvenNumber {
+
+	public static void main(String[] args) {
+		
+		int num=20;
+		
+		for (int i = 0; i < num; i++) {
+			
+			if (i%2==0) {
+			System.out.println("The oddnumbers are:" +i  );
+				
+				
+			}
+			
+			
+		}
+		
+	}
+}
+	
+
+
---
+++ PrintOddNumber.java
@@ -0,0 +1,25 @@
+package basicweb;
+
+
+public class PrintOddNumber {
+
+	public static void main(String[] args) {
+		
+		int num=20;
+		
+		for (int i = 2; i < num-1; i++) {
+			
+			if (i%2!=0) {
+			System.out.println("The Evennumbers are:" +i  );
+				
+				
+			}
+			
+			
+		}
+		
+	}
+}
+	
+
+
